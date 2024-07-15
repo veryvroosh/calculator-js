7794de023122977ff02154896a9215a9c01333a9
vroosh <zaidsafadi.2005@gmail.com>
2024-07-15 04:53:19 +0300

    added eventlisteners to all buttons, functionality in extreme WIP

--- calculator.js
+++ calculator.js
@@ -3,13 +3,45 @@ let n1, n2, op;
 function operate(n1, op, n2) {
     switch(op) {
         case "+": return n1+n2;
-            break;
         case "-": return n1-n2;
-            break;
         case "*": return n1*n2;
-            break;
         case "/": return n1/n2;
-            break;
         default: return "invalid operation";
     }
 }
+
+const display = document.querySelector("#display");
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
+
+numbers.forEach(number => number.addEventListener('click', () => {
+    console.log(`${Number(number.name)}`);
+    display.textContent += `${number.name}`
+
+
+
+}));
+
+operators.forEach(operator => operator.addEventListener('click', () => {
+    console.log(`${operator.name}`)
+
+    if(!n2) {
+        n1 = Number(display.textContent);
+        op = operator.name;
+    } else {
+        n1 = operate(n1, op, n2);
+        n2 = null;
+    }
+
+    display.textContent += `${operator.name}`;
+}));
+
+equal.addEventListener('click', () => {
+   display.textContent = operate(n1, op, n2);
+});
+
+clear.addEventListener('click', () => {
+   display.textContent = "";
+});
